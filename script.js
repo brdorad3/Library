0a77bc850a0719b3aada555995c7b4ab7fc83dcd
adis <vunicadis@gmail.com>
2023-08-28 16:17:39 +0200

    F

--- script.js
+++ script.js
@@ -5,18 +5,26 @@ function addBook(){
 let title=document.querySelector('#name').value
 let author=document.querySelector('#author').value
 let pages=document.querySelector('#pages').value
-let newBook =  new book(title, author, pages);
+let read=document.getElementById('check').checked
+let newBook =  new book(title, author, pages, read);
 library.push(newBook)
 console.log(library)
-
 }
 
-function book(title,author, pages){
+function book(title,author, pages, read){
 this.title=title;
 this.author=author;
 this.pages=pages;
+this.read=read;
 }
 
+book.prototype.toggleRead= function(){
+   this.read = !this.read
+}
+function toggleRead(index){
+library[index].toggleRead()
+addCard()
+}
 
 const all=document.querySelector('.all');
 const form=document.querySelector('#form')
@@ -36,16 +44,55 @@ function showForm(){
 }
 const add=document.querySelector('.btn2')
 
-add.addEventListener("click",(e)=>{
-   e.preventDefault()
- addBook()
- addCard()
-})
-function addCard(){
-   const card=document.querySelector('.card');
-   card.style.display="flex"
-   card.innerHTML+=`<p>${library[0].title}</p>`
-   card.innerHTML+=`<p>${library[0].author}</p>`
-   card.innerHTML+=`<p>${library[0].pages}</p>`
- 
+function signup(event){
+event.preventDefault()
+addBook()
+addCard()
 }
+
+   function remove(index){
+      library.splice(index,1)
+      addCard()
+    
+   }
+   
+   function addCard(){
+      const cards = document.querySelector('.cards');
+      cards.innerHTML = ""
+   
+      for(let i = 0; i < library.length; i++) {
+         const card = document.createElement('div');
+         card.classList = 'card';
+         card.style.display = 'grid';
+   
+         const titleRow = document.createElement('div');
+         titleRow.classList = 'titlerow';
+         titleRow.innerHTML = `<p style="text-transform: uppercase; font-size:1.35rem;">${library[i].title}</p>`;
+         titleRow.innerHTML += `<button class="close" onclick="remove(${i})">X</button>`;
+         card.appendChild(titleRow);
+   
+         const authorElement = document.createElement('p');
+         authorElement.classList = 'aut';
+         authorElement.style.alignSelf = 'start';
+         authorElement.textContent = library[i].author;
+         card.appendChild(authorElement);
+   
+         const pagesElement = document.createElement('p');
+         pagesElement.style.alignSelf = 'start';
+         pagesElement.textContent = `Pages red: ${library[i].pages}`;
+         card.appendChild(pagesElement);
+   
+         const readElement = document.createElement('p');
+         readElement.style.display="flex"
+         readElement.style.justifyContent="space-around"
+         readElement.innerHTML = library[i].read ? 'read' : 'not read';
+         readElement.innerHTML += `<button onclick="toggleRead(${i})">Toggle</button>`
+         card.appendChild(readElement);
+   
+         cards.appendChild(card);
+      }
+   }
+   
+
+
+
